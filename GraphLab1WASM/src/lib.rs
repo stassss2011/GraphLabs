e18971b0ce5f3dc7ec8fa7437bd0ab0fa10a4191
Stanislav Bondarenko <stassss2009@gmail.com>
2020-10-10 02:23:40 +0300

    almost done, except lining

--- GraphLab1WASM/src/lib.rs
+++ GraphLab1WASM/src/lib.rs
@@ -1,19 +1,223 @@
-mod utils;
-
+extern crate js_sys;
+extern crate wasm_bindgen;
+extern crate web_sys;
+use js_sys::{Float32Array, WebAssembly};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::WebGlRenderingContext;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[allow(dead_code)]
+mod utils;
+use utils::{compile_shader, link_program, set_panic_hook};
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    set_panic_hook();
+
+    Ok(())
 }
 
+#[allow(non_snake_case)]
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, GraphLab1WASM!");
+pub fn render(
+    points_arr: &JsValue,
+    lines_arr: &JsValue,
+    x_js: &JsValue,
+    y_js: &JsValue,
+) -> Result<(), JsValue> {
+    /*============ Creating a canvas =================*/
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("graph-lab-canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let gl = canvas
+        .get_context("webgl")?
+        .unwrap()
+        .dyn_into::<WebGlRenderingContext>()?;
+
+    /*==========Defining and storing the geometry=======*/
+
+    let iterator =
+        js_sys::try_iter(points_arr)?.ok_or_else(|| "need to pass iterable JS values!")?;
+    let mut vertices_mut: Vec<f32> = vec![];
+    for x in iterator {
+        vertices_mut.push(x.unwrap().as_f64().unwrap() as f32);
+    }
+    let vertices: Vec<f32> = (*vertices_mut).to_vec();
+
+    let vertices_array = {
+        let memory_buffer = wasm_bindgen::memory()
+            .dyn_into::<WebAssembly::Memory>()?
+            .buffer();
+        let location: u32 = vertices.as_ptr() as u32 / 4;
+        Float32Array::new(&memory_buffer).subarray(location, location + vertices.len() as u32)
+    };
+
+    let iterator =
+        js_sys::try_iter(lines_arr)?.ok_or_else(|| "need to pass iterable JS values!")?;
+    let mut lines_mut: Vec<f32> = vec![];
+    for x in iterator {
+        lines_mut.push(x.unwrap().as_f64().unwrap() as f32);
+    }
+    let lines: Vec<f32> = (*lines_mut).to_vec();
+
+    let lines_array = {
+        let memory_buffer = wasm_bindgen::memory()
+            .dyn_into::<WebAssembly::Memory>()?
+            .buffer();
+        let location: u32 = lines.as_ptr() as u32 / 3;
+        Float32Array::new(&memory_buffer).subarray(location, location + lines.len() as u32)
+    };
+
+
+
+    // Create an empty buffer object to store the vertex buffer
+    let vertex_buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+
+    //Bind appropriate array buffer to it
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+
+    // Pass the vertex data to the buffer
+    gl.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ARRAY_BUFFER,
+        &vertices_array,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+
+    // Unbind the buffer
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, None);
+    /*=========================Shaders========================*/
+
+    // vertex shader source code
+    let vertCode = r#"attribute vec3 aVertexPosition;
+                           uniform mat4 uMVMatrix;
+
+                           void main(void) {
+                            gl_Position = uMVMatrix * vec4(aVertexPosition, 1.5);
+                           }"#;
+    // Create a vertex shader object
+    let vertShader = compile_shader(&gl, WebGlRenderingContext::VERTEX_SHADER, vertCode)?;
+
+    // fragment shader source code
+    let fragCode = r#"void main(void) {
+                            gl_FragColor = vec4(0.7, 0.7, 1.0, 1.0);
+                           }"#;
+    // Create fragment shader object
+    let fragShader = compile_shader(&gl, WebGlRenderingContext::FRAGMENT_SHADER, fragCode)?;
+    // Link both programs
+    let shaderProgram = link_program(&gl, &vertShader, &fragShader)?;
+    // Use the combined shader program object
+    gl.use_program(Some(&shaderProgram));
+
+    /*======== Associating shaders to buffer objects ========*/
+
+    // Bind vertex buffer object
+    // gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+    // // Bind appropriate array buffer to it
+    // gl.bind_buffer(
+    //     WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+    //     Some(&Index_Buffer),
+    // );
+
+    //Bind appropriate array buffer to it
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+    // Get the attribute location
+    // let coord = gl.get_attrib_location(&shaderProgram, "coordinates") as u32;
+    let x = x_js.as_f64().unwrap() as f32;
+    let y = y_js.as_f64().unwrap() as f32;
+    let umv_matrix = gl.get_uniform_location(&shaderProgram, "uMVMatrix");
+    let mv_matrix: Vec<f32> = vec![
+        1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, y, x, 0.0, 1.0,
+    ];
+
+    gl.uniform_matrix4fv_with_f32_array(umv_matrix.as_ref(), false, &mv_matrix[..]);
+
+    // Get the attribute location
+    let coord = gl.get_attrib_location(&shaderProgram, "aVertexPosition") as u32;
+
+    // Point an attribute to the currently bound VBO
+    gl.vertex_attrib_pointer_with_i32(coord, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+
+    // Enable the attribute
+    gl.enable_vertex_attrib_array(coord);
+
+    /*============= Drawing the primitive ===============*/
+
+
+
+    // Clear the canvas
+    gl.clear_color(0.5, 0.8, 0.5, 1.0);
+
+    // // Enable the depth test
+    // gl.enable(WebGlRenderingContext::DEPTH_TEST);
+
+    // Clear the color buffer bit
+    gl.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+    // Set the view port
+    gl.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
+
+    // Draw the triangle
+    gl.draw_arrays(
+        WebGlRenderingContext::TRIANGLES,
+        0,
+        (vertices.len() / 3) as i32,
+    );
+
+
+    // fragment shader source code
+    let fragCode = r#"void main(void) {
+                            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+                           }"#;
+    // Create fragment shader object
+    let fragShader = compile_shader(&gl, WebGlRenderingContext::FRAGMENT_SHADER, fragCode)?;
+    // Link both programs
+    let shaderProgram = link_program(&gl, &vertShader, &fragShader)?;
+    // Use the combined shader program object
+    gl.use_program(Some(&shaderProgram));
+
+    let x = x_js.as_f64().unwrap() as f32;
+    let y = y_js.as_f64().unwrap() as f32;
+    let umv_matrix = gl.get_uniform_location(&shaderProgram, "uMVMatrix");
+    let mv_matrix: Vec<f32> = vec![
+        1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, y, x, 0.0, 1.0,
+    ];
+
+    // Unbind the buffer
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, None);
+    gl.delete_buffer(Some(&vertex_buffer));
+    // Create an empty buffer object to store the vertex buffer
+    let lines_buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+
+    //Bind appropriate array buffer to it
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&lines_buffer));
+
+    // Pass the vertex data to the buffer
+    gl.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ARRAY_BUFFER,
+        &lines_array,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+
+// Unbind the buffer
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, None);
+    //Bind appropriate array buffer to it
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&lines_buffer));
+    gl.uniform_matrix4fv_with_f32_array(umv_matrix.as_ref(), false, &mv_matrix[..]);
+
+    let coord = gl.get_attrib_location(&shaderProgram, "aVertexPosition") as u32;
+
+    // Point an attribute to the currently bound VBO
+    gl.vertex_attrib_pointer_with_i32(coord, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+
+    // Enable the attribute
+    gl.enable_vertex_attrib_array(coord);
+
+
+    // Draw the triangle
+    gl.draw_arrays(WebGlRenderingContext::LINES, 0, (lines.len() / 3) as i32);
+
+
+
+    Ok(())
 }
--- GraphLab1WASM/www/index.js
+++ GraphLab1WASM/www/index.js
@@ -1,3 +1,85 @@
-import * as wasm from "hello-wasm-pack";
+import {render} from "graph_lab1_wasm";
 
-wasm.greet();
+let triangles = []
+let lines = []
+
+let x = 0.15;
+let y = -0.65;
+
+let changed = false
+
+fetch('points.json')
+    .then(response => {response.json()
+            .then(res => main(res))
+    })
+
+
+function to_triangles(figures) {
+    let ret = []
+    figures.forEach(fig => {
+        if (fig.length === 9){
+            ret = ret.concat(fig)
+        } else if (fig.length === 12){
+            ret = ret.concat(fig.slice(0,9))
+            ret = ret.concat(fig.slice(3, 12))
+        }
+    })
+    return ret;
+}
+
+function to_lines(figures) {
+    let ret = []
+    figures.forEach(fig => {
+        if (fig.length === 9){
+            ret = ret.concat(fig.slice(0,6))
+            ret = ret.concat(fig.slice(3,9))
+            ret = ret.concat(fig.slice(0,3))
+            ret = ret.concat(fig.slice(6,9))
+        } else if (fig.length === 12){
+            ret = ret.concat(fig.slice(0,6))
+            ret = ret.concat(fig.slice(3,9))
+            ret = ret.concat(fig.slice(6,12))
+            ret = ret.concat(fig.slice(0,3))
+            ret = ret.concat(fig.slice(8,12))
+        }
+    })
+    return ret;
+}
+
+function main(data){
+    let figures = data.figures
+    triangles = to_triangles(figures)
+    lines = to_lines(figures)
+    console.log(triangles)
+    console.log(lines)
+    render(triangles, lines, x, y)
+}
+
+document.onkeydown = handleButtonClick;
+
+function handleButtonClick(e) {
+    switch (e.code) {
+        case 'KeyW':
+            x += 0.1
+            changed = true
+            break
+        case 'KeyS':
+            x -= 0.1
+            changed = true
+            break
+        case 'KeyA':
+            y -= 0.05
+            changed = true
+            break
+        case 'KeyD':
+            y += 0.05
+            changed = true
+            break
+    }
+
+    if (changed) {
+        render(triangles, lines, x, y)
+        changed = false
+    }
+}
+
